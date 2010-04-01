1dd99603a7f9313731349c539d32925287466e03
Jeff Tucker <trydionel@gmail.com>
2010-03-31 21:53:42 -0400

    Finishing up first draft

--- rack_upside_down_ternet.rb
+++ rack_upside_down_ternet.rb
@@ -9,21 +9,33 @@ module Rack
     end
     
     def call(env)
-      @status, @headers, @response = @app.call(env)
-      
-      document = Nokogiri::HTML(@response)
-      document.css('img').each do |image|
-        image_url = image['src']
-        if image_url =~ /(.gif|.jpg|.jpeg)/
-          filename = image_url.split('/').last
-          system "curl -o /tmp/#{filename} #{image_url}"
-          system "convert #{@effect} /tmp/#{filename} public/images/flip/#{filename}"
-          image['src'] = "/images/flip/#{filename}"
+      @env = env
+      @status, @headers, @response = @app.call(env)      
+      [@status, @headers, self]
+    end
+    
+    def each(&block)
+      @response.each do |content|
+        html = content
+        
+        if @headers['Content-Type'].include? 'text/html'
+          document = Nokogiri::HTML(content)
+          document.css('img').each do |image|
+            image_url = image['src'].gsub(/\?.+/, '')
+            if image_url =~ /(gif|jpg|jpeg|png)$/
+              filename = image_url.split('/').last
+              `curl -so tmp/#{filename} http://#{@env['HTTP_HOST']}/#{image_url}`
+              `convert tmp/#{filename} #{@effect} public/images/mod/#{filename}`
+              image['src'] = "/images/mod/#{filename}"
+            end
+          end
+          
+          html = document.to_html
+          @headers['Content-Length'] = html.length
         end
+        
+        block.call(html)
       end
-      @response = document.to_html
-      
-      [@status, @headers, @response]
     end
     
   end
